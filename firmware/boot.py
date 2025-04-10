75f72853947cc11bac10772f987babc20f0a2ae2
Sean D. Cline <sean.d.cline@gmail.com>
2025-04-09 22:47:13 -0400

    FW: Use KMK's new boot_cfg module instead of custom boot.py logic

--- firmware/boot.py
+++ firmware/boot.py
@@ -1,34 +1,23 @@
 import board
 import digitalio
-import storage
-import usb_cdc, usb_hid, usb_midi
+from kmk.bootcfg import bootcfg
 
 # Light up an LED during boot for some feedback.
 meta_led = digitalio.DigitalInOut(board.GP28)
 meta_led.switch_to_output(value=True)
 
-# Keep the mass storage device and COM port enabled when Shift is held during boot.
-# This lets Fn+Esc be a normal reset (see code.py) but Fn+Shift+Esc be a boot for programming.
-shift_col = digitalio.DigitalInOut(board.GP11)
-shift_row = digitalio.DigitalInOut(board.GP22)
-shift_col.switch_to_input(pull=digitalio.Pull.DOWN)
-shift_row.switch_to_output(value=True)
-
-if not shift_col.value:
-    storage.disable_usb_drive()
-    usb_cdc.disable()
-
-usb_midi.disable()
-
-# Let the keyboard be used in the BIOS.
-hid_devices = (usb_hid.Device.KEYBOARD, usb_hid.Device.CONSUMER_CONTROL, usb_hid.Device.MOUSE)
-usb_hid.enable(hid_devices, boot_device=1)
+# Fn+Esc is a normal reset (see code.py) but Fn+Shift+Esc will allow programming.
+bootcfg(
+    sense=board.GP22, source=board.GP11, # Right shift key during boot enables serial and usb_drive.
+    storage=False,
+    cdc_console=False,
+    midi=False,
+    boot_device=1, # Let the keyboard be used in the BIOS.
+    consumer_control=True,
+    keyboard=True,
+    mouse=True,
+)
 
 # Put things back how they were for code.py to run.
 meta_led.value = 0
 meta_led.deinit()
-shift_row.deinit()
-shift_col.deinit()
-
-# Allow writes to flash.
-storage.remount("/", False)
